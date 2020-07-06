6143e13023b1a8b89a486710d3b67c36080f3766
RomanSaburski <romansaburski@gmail.com>
2020-07-06 22:06:49 +0300

    Доработка

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 var buttonSearch = document.querySelector('.button-popup');
 var windowPopup = document.querySelector('.popup-element');
-console.log(buttonSearch);
 buttonSearch.addEventListener("click", function(evt) {
     evt.preventDefault();
-    windowPopup.classList.toggle('modal-popup')
+    windowPopup.classList.toggle('modal-popup');
 });
