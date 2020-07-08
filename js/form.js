00ebe84e562c73fbb554ddfdc22b64cfc411265a
RomanSaburski <romansaburski@gmail.com>
2020-07-08 18:10:11 +0300

    Исправление недочетов к защите

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 var buttonSearch = document.querySelector('.button-popup');
 var windowPopup = document.querySelector('.popup-element');
-buttonSearch.addEventListener("click", function(evt) {
+buttonSearch.addEventListener('click', function(evt) {
     evt.preventDefault();
     windowPopup.classList.toggle('modal-popup');
 });
